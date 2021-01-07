b3cf3fb6d100ff8203a91ccd009dce6177da8158
彭成刚 <76526211@qq.com>
2021-01-07 16:31:01 +0800

    up

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,15 +1,8 @@
 const argv = require('minimist')(process.argv.slice(2))
-console.info('argv', argv)
 const build = argv.build || false
-// base: '/vitepress2021/',
-// base: command === 'build' ? '/vitepress2021/' : '/',
-// base: build ? '/vitepress2021/' : '/',
-
-console.info('build', build)
-const base1 = build ? '/vitepress2021/' : '/'
+const baseBuild  = build ? '/vitepress2021/' : '/'
 module.exports = {
-  base: base1,
-  // base: '/',
+  base: baseBuild ,
   title: 'Hello VitePress',
   description: 'Just playing around.'
 }
