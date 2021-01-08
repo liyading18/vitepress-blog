94019540b988df5da46b5ea036dbfc956b895f10
彭成刚 <76526211@qq.com>
2021-01-08 11:29:03 +0800

    up

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -25,7 +25,7 @@ module.exports = {
 function getSidebar () {
   return {
     '/config/': 'auto',
-    '/VitePress/': [{ 
+    '/VitePress/': [{
         text: 'GUI', 
         children:[
           { text: '主要问题', link: '/VitePress/' },
