b66c0462d99495fc2276bfbd160799ef5a6a2657
彭成刚 <76526211@qq.com>
2021-01-08 14:35:09 +0800

    up

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,14 +4,23 @@ const baseBuild  = build ? '/vitepress2021/' : '/'
 
 function getNav () {
   return [
-    { text: '主要页面', link: '/VitePress/'},
+    { text: '主要页面', link: '/VitePress/one'},
     { text: '工作相关', link: '/work'},
     { text: 'Config', link: '/config'},
-    { text: 'Navigator导航', link: "http://pengchenggang.gitee.io/navigator/" }
+    { text: 'Navigator导航', link: "http://pengchenggang.gitee.io/navigator/" },
+    {
+        text: '下拉菜单',
+        // icon: 'reco-document',
+        items: [
+            { text: 'work1', link: '/work' },
+            { text: 'config', link: '/config' },
+        ]
+    },
   ]
 }
 
 module.exports = {
+  hmr: { overlay: false },
   base: baseBuild,
   title: 'Hello VitePress',
   description: 'Just playing around.',
@@ -28,8 +37,9 @@ function getSidebar () {
     '/VitePress/': [{
         text: 'GUI', 
         children:[
+          { text: '主要问题', link: '/VitePress/one' },
           { text: 'second', link: '/VitePress/second' },
-          { text: '主要问题', link: '/VitePress/' },
+          { text: 'index', link: '/VitePress/' },
         ]
       }
     ]
---
+++ docs/VitePress/one.md
@@ -0,0 +1,24 @@
+# VitePress Index
+```
+![An image](./images/VitePressPng.png)
+```
+![An image](./images/VitePressPng.png)
+
+```
+<img :src="$withBase('/images/VitePressPng.png')" alt="foo">
+```
+<img :src="$withBase('/images/VitePressPng.png')" alt="foo">
+
+<!-- [VitePress](./VitePress/) -->
+
+## test
+[在线地址](http://pengchenggang.gitee.io/VitePress2021/)
+
+[发布地址](https://gitee.com/pengchenggang/vitepress2021/pages)
+
+## test11
+{{ $page.title }}
+
+```
+{{ $page.title }}
+```
